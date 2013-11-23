ac4f68e038b7a231fbf3ec269141cb21976f972b
陈袁 <chengcheng222e@sina.com>
2013-11-23 14:02:08 +0800

    添加了不同环境的配置文件,以及几个工具类

---
+++ src/main/java/com/vernon/netty/core/common/util/DataUtil.java
@@ -0,0 +1,11 @@
+package com.vernon.netty.core.common.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenyuan
+ * Date: 13-11-23
+ * Time: PM1:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataUtil {
+}
