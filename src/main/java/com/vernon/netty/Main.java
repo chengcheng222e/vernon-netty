a028e8fd3f9441c6caff0da4c81ed680809219a4
陈袁 <chengcheng222e@sina.com>
2014-03-26 21:49:52 +0800

    Mac提交

--- src/main/java/com/vernon/netty/Main.java
+++ src/main/java/com/vernon/netty/Main.java
@@ -20,6 +20,7 @@ public class Main {
     public static void main(String[] args) {
         HttpServer httpServer = new HttpServer();
         httpServer.start();
+
         LOGGER.info("--------------------------- httpServer start ---------------------------");
     }
 
